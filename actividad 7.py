0b4350441dd063a674be8c29ba4a03dc3a93f410
Agustin20004 <Agustin.petrussa.10@gmail.com>
2025-08-17 03:41:21 -0300

    Create actividad 7.py

---
+++ actividad 7.py
@@ -0,0 +1,12 @@
+numero1 = int (input ("Ingrese un numero entero que no sea 0"))
+numero2 = int (input ("Ingrese otro numero entero que no sea 0"))
+
+print (f"La suma de los numeros es:{numero1+numero2}")
+
+print (f"La division de los numeros es:{numero1/numero2}")
+
+print (f"La multiplicacion de los numeros es:{numero1*numero2}")
+
+print (f"La resta de los numeros es:{numero1-numero2}")
+
+print ("Fin de la operacion")
