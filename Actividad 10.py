fd02d3d8fa3cd02d9b374be82e3f316157de4055
Agustin20004 <Agustin.petrussa.10@gmail.com>
2025-08-17 03:41:27 -0300

    Create Actividad 10.py

---
+++ Actividad 10.py
@@ -0,0 +1,7 @@
+numero1 = int ( input("Ingrese un numero"))
+numero2 = int ( input("Ingrese otro numero"))
+numero3 = int ( input("Ingrese el ultimo numero"))
+
+promedio = ( (numero1 + numero2 + numero3) / 3)
+
+print (F"El promedio de los numeros ingresados es {promedio}")
