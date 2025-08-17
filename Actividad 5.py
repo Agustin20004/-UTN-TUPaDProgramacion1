d8ea7a9067f47dda404e347ff2e8762dd1ff28a3
Agustin20004 <Agustin.petrussa.10@gmail.com>
2025-08-17 03:41:17 -0300

    Create Actividad 5.py

---
+++ Actividad 5.py
@@ -0,0 +1,7 @@
+segundos = int (input("Ingrese cantidad de segundos"))
+
+print (f"la cantidad de segundos es {segundos}")
+
+horas = (segundos / 3600)
+
+print(f"La cantidad de segundos en horas es {horas}")
