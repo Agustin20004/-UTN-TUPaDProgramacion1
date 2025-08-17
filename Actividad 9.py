e0605a7e6d557150166c8add04d3c28a146edb21
Agustin20004 <Agustin.petrussa.10@gmail.com>
2025-08-17 03:41:25 -0300

    Create Actividad 9.py

---
+++ Actividad 9.py
@@ -0,0 +1,7 @@
+gradocelsius = int (input("Ingrese la temperatura en grado celsius"))
+
+print (f"La temperatura en grado celsius es {gradocelsius}")
+
+tempfahrenheit = (9 / 5 * gradocelsius + 32)
+
+print(f"La temperatura en Fahrenheit es {tempfahrenheit}")
